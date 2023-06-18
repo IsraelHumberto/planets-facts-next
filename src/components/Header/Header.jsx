077e42cfce41f4014ad48eccf290952c70f14e5b
IsraelHumbertoLP <israel.pereira@corebiz.ag>
2023-06-18 01:27:57 -0300

    feat: create header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,67 @@
 'use client'
+import { useEffect, useState } from 'react'
 import styles from './Header.module.scss'
+import { BsList, BsChevronRight } from 'react-icons/bs'
+
 
 const Header = ({ linksHeader }) => {
+    const [openSidebar, setOpenSidebar] = useState(false)
 
-    const renderLinks = linksHeader.map((link, index) => (
-        <button key={index}>
-            {link.name}
-        </button>
-    ))
+    const renderLinksDesk = linksHeader.map((link, index) => {
+        return (
+            <button key={index} className='h3'>
+                {link.name}
+            </button>
+        )
+    })
+    const renderLinksMobile = linksHeader.map((link, index) => {
+        return (
+            <div key={index} className={styles.navItem}>
+                <button className='h3'>
+                    <div>
+                        <span className={styles.circle} style={{ background: link.color }}></span>
+                        <span className={styles.text}>
+                            {link.name}
+                        </span>
+                        <span className={styles.arrow}>
+                            <BsChevronRight fontSize={12} color='#FFF' />
+                        </span>
+                    </div>
+                </button>
+            </div>
+        )
+    })
+
+    const Sidebar = () => {
+        return (
+            <div
+                className={`${styles.sidebar} ${openSidebar ? `${styles.active}` : ''}`}
+            >         <div className={styles.navMobile}>
+                    {renderLinksMobile}
+                </div>
+            </div>
+        )
+    }
 
     return (
         <>
             <header className={styles.container}>
                 <div className={styles.content}>
-                    <div className={`${styles.logo} h2`}>
+                    <div className={`${styles.logo}`}>
                         The Planets
                     </div>
-                    <nav className={styles.nav}>
-                        {renderLinks}
+                    <nav className={styles.navDesktop}>
+                        {renderLinksDesk}
                     </nav>
+                    <button className={styles.iconHamburguer}>
+                        <BsList
+                            fontSize={28}
+                            color={'#FFF'}
+                            onClick={() => setOpenSidebar(!openSidebar)}
+                        />
+                    </button>
                 </div>
+                <Sidebar />
             </header>
         </>
     )
